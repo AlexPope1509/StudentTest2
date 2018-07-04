a73fdff2b78960ff7210c368d986b514e340c11a
Mona Eghbali <Mona.Eghbali@csi-europe.com>
2018-07-04 15:30:45 +0100

    first commit

---
+++ myTestApp2/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myTestApp2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name  { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}
